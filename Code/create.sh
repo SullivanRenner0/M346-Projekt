1de05c0cea53cb43a3ae95e0459ad521405d6ee0
DESKTOP-FB3SAQO\renne <sullivan.renner@edu.gbssg.ch>
2022-12-17 17:58:22 +0100

    Bash update (funktioniert)

--- Code/create.sh
+++ Code/create.sh
@@ -9,15 +9,18 @@ sudo apt-get install -qq jq > /dev/null
 echo "Packages wurden fertig vorbereitet"
 
 #Variabeln deklarieren
-export bucket1=sr-lg-m346-projekt-source-2
-export bucket2=sr-lg-m346-projekt-compressed-2
-export functionName=copyImage2
-export zipName=lambda.zip
+export bucket1o="sr-lg-m346-projekt-original"
+export bucket2o="sr-lg-m346-projekt-compressed"
+export bucket1=""
+export bucket2=""
+export functionNameo="copyImage"
+export functionName=""
+export zipName="lambda.zip"
 export funcLayer="arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pillow:1" #PIL - Image
 export compressesPrefix="resized_"
 export py="index.py"
-export pyTemp="CopyImage.py"
 export pyTempName="CopyImage"
+export pyTemp="$pyTempName.py"
 export accountNumber=''
 export policy=''
 export eventJson=''
@@ -25,11 +28,18 @@ export testBildName=''
 export testBildDir=''
 export filesInTestBildDir=''
 
-
-
-#Buckets löschen
-aws s3 rb s3://$bucket1 --force > /dev/null
-aws s3 rb s3://$bucket2 --force > /dev/null
+#Einzigartige Namen für die Buckets und die Funktion finden
+bucket1=$bucket1o
+bucket2=$bucket2o
+functionName=$functionNameo
+export i=0
+while `aws s3api head-bucket --bucket $bucket1 2>/dev/null || aws s3api head-bucket --bucket $bucket2 2>/dev/null || aws lambda get-function --function-name $functionName 2>/dev/null`
+do
+i=$((i+1))
+bucket1="$bucket1o-$i"
+bucket2="$bucket2o-$i"
+functionName=$functionNameo"_"$i
+done
 
 #Buckets erstellen
 echo ""
@@ -37,9 +47,6 @@ aws s3 mb s3://$bucket1 > /dev/null
 echo "Bucket \"$bucket1\" wurde erstellt"
 aws s3 mb s3://$bucket2 > /dev/null
 echo "Bucket \"$bucket2\" wurde erstellt"
-
-#Funktion löschen
-aws lambda delete-function --function-name $functionName > /dev/null
 
 #Funktion erstellen
 cp $py $pyTemp > /dev/null
@@ -50,11 +57,12 @@ zip -r9 $zipName $pyTemp > /dev/null
 rm $pyTemp
 accountNumber=`aws sts get-caller-identity | jq -r '.Account'`
 aws lambda create-function --function-name "$functionName" --runtime python3.9 --zip-file "fileb://$zipName" --handler "$pyTempName.lambda_handler" --role "arn:aws:iam::"$accountNumber":role/LabRole" --region us-east-1 --layers "$funcLayer" > /dev/null
-echo "Lambda Funktion \"$functionName\" wurde erstellt"
 rm $zipName
 
 #Der Funktion das Recht geben  aud den Bucket zuzugreifen
 aws lambda add-permission --function-name $functionName --action lambda:InvokeFunction --statement-id s3 --principal s3.amazonaws.com --source-arn "arn:aws:s3:::$bucket1" --output text > /dev/null
+
+echo "Lambda Funktion \"$functionName\" wurde erstellt"
 
 #Policy des Buckets erstellen
 policy='{
@@ -74,7 +82,7 @@ policy='{
 }'
 aws s3api put-bucket-policy --bucket "$bucket1" --policy "$policy" > /dev/null
 
-#Dem Bucket einen Trigger(alle Create Evente) hinzufügen und auf die Funktion verweisen 
+#Dem Bucket einen Trigger(alle Create Events) hinzufügen und auf die Funktion verweisen 
 eventJson='{
     "LambdaFunctionConfigurations": 
     [
@@ -107,9 +115,11 @@ aws s3api put-bucket-notification-configuration --bucket "$bucket1" --notificati
 #Ausgabe und Beispiel
 echo ""
 echo "Alles fertig eingerichtet"
+echo "Bucket orginal: "$bucket1
+echo "Bucket verkleinert: "$bucket2
 echo ""
 filesInTestBildDir=(../Testbild/*)
-testBildPfad="${filesInTestBildDir[0]}"
+testBildPfad="${filesInTestBildDir[-1]}"
 if [ -z "$testBildPfad" ]
 then
     echo "Im \"Testbild-Ordner ist keine Datei\""
@@ -123,10 +133,26 @@ else
     { set +x; } 2>/dev/null
     sleep 5 # Der Copy-Funktion zeit geben da sie ein paar Sekunden braucht
     set -x
-    aws s3 cp "s3://$bucket2/$compressesPrefix$testBildName" "$testBildDir/$compressesPrefix$testBildName"
+    if `aws s3 cp "s3://$bucket2/$compressesPrefix$testBildName" "$testBildDir/$compressesPrefix$testBildName" >/dev/null`
+    then
     { set +x; } 2>/dev/null
 
+    #Bild wieder von Buckets entfernen
+    aws s3 rm "s3://$bucket1/$testBildName" > /dev/null
+    aws s3 rm "s3://$bucket2/$compressesPrefix$testBildName" > /dev/null
+
     echo ""
-    echo "Erfolg :)"
     echo "Das verkleinerte Bild ist gespeicher unter: $testBildDir/$compressesPrefix$testBildName"
+    echo "Erfolg :)"
+    else
+    { set +x; } 2>/dev/null
+    
+    #Bild wieder von Buckets entfernen
+    aws s3 rm "s3://$bucket1/$testBildName" > /dev/null
+    aws s3 rm "s3://$bucket2/$compressesPrefix$testBildName" > /dev/null
+
+    echo ""
+    echo "Es ist ein Fehler aufgetreten :("
+
+    fi
 fi
